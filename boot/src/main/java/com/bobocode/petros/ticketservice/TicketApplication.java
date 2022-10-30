a779e1c53214e7353408a6e3eefce656e1b31ea8
Oleksii Talakh <oleksii.talakh@gmail.com>
2022-10-30 09:17:44 +0200

    fix maven shit

--- boot/src/main/java/com/bobocode/petros/ticketservice/TicketApplication.java
+++ boot/src/main/java/com/bobocode/petros/ticketservice/TicketApplication.java
@@ -2,9 +2,7 @@ package com.bobocode.petros.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class TicketApplication {
     public static void main(String[] args) {
